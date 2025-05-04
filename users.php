088f472376c37e0a036efcd4a47cab74736665f7
songorren02 <david.amador@enti.cat>
2025-05-04 22:51:53 +0200

    users.php acabado

---
+++ users.php
@@ -0,0 +1,68 @@
+<?php
+require_once("func.check_session.php");
+
+#Comprobar sesion
+$session = check_session();
+
+require_once("template.php");
+
+#Abrir html
+open_html();
+
+#Comprobar cierre de sesion
+if (isset($_COOKIE["logout"]) && $_COOKIE["logout"] != ""){
+	$user_prev = addslashes($_COOKIE["logout"]);
+	setcookie("logout", "");
+	echo <<<EOD
+<p id="logout_msg" class="notice"><strong>Session del usuario <em>{$user_prev}</em> cerrada</strong></p>
+EOD;
+}
+
+#Conexion db
+require_once("db_conf.php");
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_db);
+
+#Obtener usuarios
+
+$query = <<<EOD
+SELECT
+	username
+FROM
+	users
+EOD;
+
+$resultado = mysqli_query($conn, $query);
+
+#Comprobar si hay mensajes
+if(!$resultado){
+	echo "<p class=\"error_msg\">Error al leer los usuarios</p>";
+	echo <<<EOD
+</section>
+EOD;
+	close_html();
+	exit();
+}
+
+
+echo <<<EOD
+<ul class="users">
+<h2>Usuarios</h2>
+EOD;
+
+#Mostrar los usuarios
+while($user = $resultado->fetch_assoc()){
+	echo <<<EOD
+	<li>{$user["username"]}</li>
+EOD;
+}
+
+#Cierres de html
+echo <<<EOD
+</section>
+EOD;
+
+close_html();
+
+
+?>
